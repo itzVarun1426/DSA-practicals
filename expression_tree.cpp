3cee56e75021258eda6ac324c854417718c29257
Varun N <114867149+itzVarun1426@users.noreply.github.com>
2025-04-15 14:19:27 +0530

    Update expression_tree.cpp

--- expression_tree.cpp
+++ expression_tree.cpp
@@ -1,6 +1,4 @@
-/*
-Experiment 7 : Construct an expression tree from the given prefix and traverse it using post order traversal and then delete the entire tree.
-*/
+
 #include <iostream>
 #include <string.h>
 using namespace std;
